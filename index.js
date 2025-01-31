59b91117cbab607a7040a59b87df06ab8bd627fa
ABIR ISLAM <abirislam099@outlook.com>
2025-01-31 17:20:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function startBot(message) {
 
 startBot();
 
-const currentVersion = '1.0.9';
+const currentVersion = '1.1.1';
 let isApiEnabled = true; // Flag to track if API is enabled
 
 async function checkForUpdates() {
